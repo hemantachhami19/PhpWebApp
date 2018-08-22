bea56ab48ce0a5f18e7d0713e5bd795c720cf492
Hemant Achhami <hemantachhami19@gmail.com>
2018-08-22 06:26:24 +0545

    Separating html from php logic basic

--- index.php
+++ index.php
@@ -1,30 +1,4 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+<?php
+$name =isset($_GET['name'])? $_GET['name']:"anybody";
+require "index.view.php";
 
-    <style>
-        header{
-            background-color: red;
-            padding: 2px;
-            text-align: center;
-        }
-    </style>
-
-
-</head>
-<body>
-    <header>
-        <h1>
-            <?php
-                $name =isset($_GET['name'])? $_GET['name']:"anybody";
-                echo "Hello {$name}";
-            ?>
-        </h1>
-    </header>
-</body>
-</html>
