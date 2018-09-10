00958d9be7a899874c1ac2007f4ba358f68828c4
Hemant Achhami <hemantachhami19@gmail.com>
2018-09-10 06:55:30 +0545

    Basic todo done

--- index.php
+++ index.php
@@ -1,4 +1,35 @@
 <?php
 $name =isset($_GET['name'])? $_GET['name']:"anybody";
+
+
+Class Task{
+    public $description,$completed;
+    public function __construct($description,$completed =true)
+    {
+        $this->description = $description;
+        $this->completed =$completed;
+    }
+
+    public function isComplete()
+    {
+         return $this->completed;
+    }
+
+    public function complete()
+    {
+        $this->completed = true;
+    }
+}
+
+$tasks =[
+    new Task('Buy laptop1',false),
+    new Task('Buy laptop2'),
+    new Task('Buy laptop3')
+];
+
 require "index.view.php";
+
+
+
+
 
--- index.view.php
+++ index.view.php
@@ -20,5 +20,19 @@
 <header>
     <h1>Hello <?php echo $name;?></h1>
 </header>
+
+<ul>
+    <?php foreach ($tasks as $task):?>
+        <li>
+
+                <?php if($task->completed):?>
+                <strike><?php echo $task->description?></strike>
+                <?php else: ?>
+                <?php echo $task->description?>
+                <?php endif;?>
+
+        </li>
+    <?php endforeach?>
+</ul>
 </body>
 </html>
