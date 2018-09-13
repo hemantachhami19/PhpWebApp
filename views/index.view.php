1d39e4cb75c0ed17c1eda69957a21f44c2e6e7b1
Hemant Achhami <hemantachhami19@gmail.com>
2018-09-13 07:54:40 +0545

    drying up the templates

--- views/index.view.php
+++ views/index.view.php
@@ -1,21 +1,6 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-<header>
-    <nav>
-        <ul>
-            <li><a href="about">About Us</a> </li>
-            <li><a href="contact">Contact Us</a> </li>
-        </ul>
-    </nav>
-</header>
+<?php
+require 'views/partials/head.php';
+?>
 
 <ul>
     <?php foreach ($tasks as $task):?>
@@ -30,5 +15,4 @@
         </li>
     <?php endforeach?>
 </ul>
-</body>
-</html>
+<?php require 'views/partials/foot.php'?>
---
+++ views/partials/nav.php
@@ -0,0 +1,7 @@
+<nav>
+    <ul>
+        <li><a href="/about">About Us</a> </li>
+        <li><a href="/contact">Contact Us</a> </li>
+        <li><a href="about/culture">About our culture</a> </li>
+    </ul>
+</nav>
